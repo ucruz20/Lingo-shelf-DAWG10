f0f0180d7563eb462a46548d83c4bfdb909d8140
Pablo Hurtado <pdhchurtado@gmail.com>
2026-05-17 22:53:15 -0600

    fix:merge error

--- backend/proyecto/src/main/java/com/lingoshelf/proyecto/dto/BookResponse.java
+++ backend/proyecto/src/main/java/com/lingoshelf/proyecto/dto/BookResponse.java
@@ -1,10 +1,7 @@
 package com.lingoshelf.proyecto.dto;
 
-<<<<<<< HEAD
-=======
 import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(name = "BookResponse", description = "Respuesta resumida de un libro")
->>>>>>> ff25157f6ba6e728615e27b4fbabccabfbf1977a
 public record BookResponse(Long id, String title, Double price) {
 }
