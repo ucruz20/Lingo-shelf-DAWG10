d1e932d6d8a5cadebdcc61a97b7263645005746c
KennetCastro <cs24010@ues.edu.sv>
2026-06-11 20:33:47 -0600

    fix: nombres de tabla usaban palablas reservadas de postgres

--- backend/proyecto/src/main/java/com/lingoshelf/proyecto/entity/Order.java
+++ backend/proyecto/src/main/java/com/lingoshelf/proyecto/entity/Order.java
@@ -10,12 +10,14 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
+@Table(name = "orders")
 @Getter
 @Setter
 @NoArgsConstructor
